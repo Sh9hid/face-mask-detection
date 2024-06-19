0a6d65efea4132131c73928eefeb1f8c1d780e78
Abdullah Shahid Khan <69131903+Sh9hid@users.noreply.github.com>
2024-06-19 12:45:11 +0530

    Update prepare.py

--- utils/prepare.py
+++ utils/prepare.py
@@ -5,20 +5,29 @@ from sklearn.model_selection import train_test_split
 from PIL import Image
 
 def split_and_copy_images(annotation_path, images_path, output_path):
-    # Load annotation data
+    '''
+    Split images into train and val sets 
+    and set up directory in correct format 
+    for classification training.
+    :params annotation_path\csv
+    :params path to all images for training
+    :return
+    '''
+    # STEP 1: Load the CSV file.
     annotations = pd.read_csv(annotation_path)
 
-    # Split data into training and validation sets
+    # STEP 2: Split data into 80/20 training and validation set respectively.
     train_df, val_df = train_test_split(annotations, test_size=0.2, random_state=42)
 
+    # STEP 3: Set train and val paths
     TRAIN_IMAGES_DIR = os.path.join(output_path, 'train')
     VAL_IMAGES_DIR = os.path.join(output_path, 'val')
 
-    # Get lists of images in each split
+    # STEP 4: Make a list of images for copying from earlier split. 
     train_images = list(train_df['name'])
     val_images = list(val_df['name'])
 
-    # Create directories for training and validation data for Classification Model.
+    # STEP 5: Set up directory-path, key-value pairs for improving readability 
     directories = {
         'train_mask': os.path.join(TRAIN_IMAGES_DIR, 'mask'),
         'train_no_mask': os.path.join(TRAIN_IMAGES_DIR, 'no-mask'),
